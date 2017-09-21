fe0cc7318e72514334057ef7deef4613790ef8c9
cptbtptpe <1159882790@qq.com>
2017-09-21 11:33:11 +0800

    git add frontend/views/producer/product-list.php

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -49,6 +49,7 @@ $shareCover = empty($params['share_cover']) ? $params['frontend_source'] . '/img
 </script>
 
 <body<?= $ngCtl ?>>
+<<<<<<< Updated upstream
 
 <div class="all-lm">
     <!-- Loading -->
@@ -143,18 +144,115 @@ $shareCover = empty($params['share_cover']) ? $params['frontend_source'] . '/img
             <a href="<?= Url::to(['producer/index']) ?>" class="hr">
                 <img src="<?= $params['frontend_source'] ?>/img/producer.svg"/>
                 分销管理
-            </a>
-        <?php endif; ?>
-        <a href="tel:<?= $params['company_tel'] ?>" class="hr">
-            <img src="<?= $params['frontend_source'] ?>/img/phone.svg"/>
-            咨询客服
-        </a>
-        <a href="<?= SsoClient::$ssoHost ?><?= Url::toRoute(['auth/logout']) ?>" class="hr">
-            <img src="<?= $params['frontend_source'] ?>/img/exit.svg"/>
-            退出登录
-        </a>
+=======
+    <div class="all-lm">
+        <!-- Loading -->
+        <div id="loading" class="kk-animate kk-show hidden">
+            <div class="loading-bar loading-bounce kk-animate kk-t2b-show">
+                <div class="in"></div>
+                <div class="out"></div>
+            </div>
+        </div>
+
+        <!-- Message -->
+        <div id="message" class="kk-animate kk-show hidden">
+            <div class="message-bar kk-animate kk-t2b-show">
+                <p class="message-box"></p>
+            </div>
+        </div>
+
+        <!-- Hit -->
+        <div id="hit" class="hidden">
+            <div class="hit-bar hit-bounce kk-animate">
+                <div class="in"></div>
+                <div class="out"></div>
+            </div>
+        </div>
+
+        <!-- Body -->
+        <?php $this->beginBody() ?>
+        <?= $content ?>
+        <?php $this->endBody() ?>
+
+        <?php
+        $minDirectory = (YII_ENV == 'dev' ? null : '_min');
+        $suffix = (YII_ENV == 'dev' ? time() : VERSION);
+
+        $sourceUrl = $params['frontend_source'];
+        $items = [
+            'css',
+            'js'
+        ];
+        foreach ($items as $item) {
+            $variable = 'source' . ucfirst($item);
+            $register = 'register' . ucfirst($item) . 'File';
+
+            if (is_null($this->context->{$variable}) || 'auto' == $this->context->{$variable}) {
+                $source = "/{$item}{$minDirectory}/{$controller}/{$action}.{$item}";
+                $this->{$register}($sourceUrl . $source . "?version=" . $suffix);
+            } elseif (is_array($this->context->{$variable})) {
+                foreach ($this->context->{$variable} as $value) {
+                    if (strpos($value, '/') === 0) {
+                        $source = "${sourceUrl}{$value}.{$item}";
+                    } else if (strpos($value, 'http:') === 0 || strpos($value, 'https:') === 0) {
+                        $source = $value;
+                    } else {
+                        $source = "${sourceUrl}/{$item}{$minDirectory}/{$value}.{$item}";
+                    }
+
+                    $char = strpos($source, '?') !== false ? '&' : '?';
+                    $this->{$register}($source . $char . "version=" . $suffix);
+                }
+            }
+        }
+        ?>
+
+        <!-- Footer -->
+        <div class="hidden">
+            <span ng-init="common({message: '<?= $app->session->getFlash("message") ?>'})"></span>
+            <span ng-init='wxSDK(<?= Yii::$app->wx->js->config([
+                'hideMenuItems',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ]) ?>, "<?= $shareTitle ?>", "<?= $shareDescription ?>", "<?= $shareCover ?>")'></span>
+        </div>
     </div>
+
+    <!-- Menu -->
+    <div class="menu-lm">
+        <!-- Triangle -->
+        <!-- <div class="triangle"></div> -->
+        <div>
+            <p class="menuclose">KAKE</p>
+            <a href="<?= Url::to(['site/index']) ?>">
+                <img src="<?= $params['frontend_source'] ?>/img/site.svg"/>
+                首页
+            </a>
+            <a href="<?= Url::to(['order/index']) ?>" class="hr">
+                <img class="order-center" src="<?= $params['frontend_source'] ?>/img/order-center.svg"/>
+                订单中心
+            </a>
+            <?php if (!empty($this->params['user_info']->role) && $this->params['user_info']->role <= 10): ?>
+                <a href="<?= Url::to(['producer/index']) ?>" class="hr">
+                    <img src="<?= $params['frontend_source'] ?>/img/producer.svg"/>
+                    分销管理
+                </a>
+            <?php endif; ?>
+            <a href="tel:<?= $params['company_tel'] ?>" class="hr">
+                <img src="<?= $params['frontend_source'] ?>/img/phone.svg"/>
+                咨询客服
+>>>>>>> Stashed changes
+            </a>
+            <a href="<?= SsoClient::$ssoHost ?><?= Url::toRoute(['auth/logout']) ?>" class="hr">
+                <img src="<?= $params['frontend_source'] ?>/img/exit.svg"/>
+                退出登录
+            </a>
+        </div>
+    </div>
+<<<<<<< Updated upstream
 </div>
+=======
+>>>>>>> Stashed changes
 
 </body>
 <script>
