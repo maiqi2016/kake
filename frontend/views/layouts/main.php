3777135e6c4173406d4ad71395301b794aea7bb3
Leon <jiangxilee@gmail.com>
2017-09-27 17:50:22 +0800

    common_modify_less

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -34,7 +34,7 @@ $shareCover = empty($params['share_cover']) ? $params['frontend_source'] . '/img
 <!DOCTYPE html>
 <html ng-app="<?= $ngApp ?>" lang="<?= $app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+    <meta charset="<?= $app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="keywords" content="<?= $keywords ?>">
     <meta name="description" content="<?= $description ?>">
@@ -43,7 +43,7 @@ $shareCover = empty($params['share_cover']) ? $params['frontend_source'] . '/img
     <?php $this->head() ?>
 </head>
 <script type="text/javascript">
-    function setRem(){
+    function setRem() {
         // 设置rem
         document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px';
     }
@@ -56,135 +56,114 @@ $shareCover = empty($params['share_cover']) ? $params['frontend_source'] . '/img
 </script>
 
 <body<?= $ngCtl ?>>
-    <div class="all-lm">
-        <!-- Loading -->
-        <div id="loading" class="kk-animate kk-show hidden">
-            <div class="loading-bar loading-bounce kk-animate kk-t2b-show">
-                <div class="in"></div>
-                <div class="out"></div>
-            </div>
-        </div>
 
-        <!-- Message -->
-        <div id="message" class="kk-animate kk-show hidden">
-            <div class="message-bar kk-animate kk-t2b-show">
-                <p class="message-box"></p>
-            </div>
-        </div>
+<!-- Loading -->
+<div id="loading" class="kk-animate kk-show hidden">
+    <div class="loading-bar loading-bounce kk-animate kk-t2b-show">
+        <div class="in"></div>
+        <div class="out"></div>
+    </div>
+</div>
 
-        <!-- Hit -->
-        <div id="hit" class="hidden">
-            <div class="hit-bar hit-bounce kk-animate">
-                <div class="in"></div>
-                <div class="out"></div>
-            </div>
-        </div>
+<!-- Message -->
+<div id="message" class="kk-animate kk-show hidden">
+    <div class="message-bar kk-animate kk-t2b-show">
+        <p class="message-box"></p>
+    </div>
+</div>
 
-        <!-- Body -->
-        <?php $this->beginBody() ?>
-        <?= $content ?>
-        <?php $this->endBody() ?>
+<!-- Hit -->
+<div id="hit" class="hidden">
+    <div class="hit-bar hit-bounce kk-animate">
+        <div class="in"></div>
+        <div class="out"></div>
+    </div>
+</div>
 
-        <?php
-        $minDirectory = (YII_ENV == 'dev' ? null : '_min');
-        $suffix = (YII_ENV == 'dev' ? time() : VERSION);
+<!-- Body -->
+<?php $this->beginBody() ?>
+<?= $content ?>
+<?php $this->endBody() ?>
 
-        $sourceUrl = $params['frontend_source'];
-        $items = [
-            'css',
-            'js'
-        ];
-        foreach ($items as $item) {
-            $variable = 'source' . ucfirst($item);
-            $register = 'register' . ucfirst($item) . 'File';
+<?php
+$minDirectory = (YII_ENV == 'dev' ? null : '_min');
+$suffix = (YII_ENV == 'dev' ? time() : VERSION);
 
-            if (is_null($this->context->{$variable}) || 'auto' == $this->context->{$variable}) {
-                $source = "/{$item}{$minDirectory}/{$controller}/{$action}.{$item}";
-                $this->{$register}($sourceUrl . $source . "?version=" . $suffix);
-            } elseif (is_array($this->context->{$variable})) {
-                foreach ($this->context->{$variable} as $value) {
-                    if (strpos($value, '/') === 0) {
-                        $source = "${sourceUrl}{$value}.{$item}";
-                    } else if (strpos($value, 'http:') === 0 || strpos($value, 'https:') === 0) {
-                        $source = $value;
-                    } else {
-                        $source = "${sourceUrl}/{$item}{$minDirectory}/{$value}.{$item}";
-                    }
+$sourceUrl = $params['frontend_source'];
+$items = [
+    'css',
+    'js'
+];
+foreach ($items as $item) {
+    $variable = 'source' . ucfirst($item);
+    $register = 'register' . ucfirst($item) . 'File';
 
-                    $char = strpos($source, '?') !== false ? '&' : '?';
-                    $this->{$register}($source . $char . "version=" . $suffix);
-                }
+    if (is_null($this->context->{$variable}) || 'auto' == $this->context->{$variable}) {
+        $source = "/{$item}{$minDirectory}/{$controller}/{$action}.{$item}";
+        $this->{$register}($sourceUrl . $source . "?version=" . $suffix);
+    } elseif (is_array($this->context->{$variable})) {
+        foreach ($this->context->{$variable} as $value) {
+            if (strpos($value, '/') === 0) {
+                $source = "${sourceUrl}{$value}.{$item}";
+            } else if (strpos($value, 'http:') === 0 || strpos($value, 'https:') === 0) {
+                $source = $value;
+            } else {
+                $source = "${sourceUrl}/{$item}{$minDirectory}/{$value}.{$item}";
             }
+
+            $char = strpos($source, '?') !== false ? '&' : '?';
+            $this->{$register}($source . $char . "version=" . $suffix);
         }
-        ?>
+    }
+}
+?>
 
-        <!-- Footer -->
-        <div class="hidden">
-            <span ng-init="common({message: '<?= $app->session->getFlash("message") ?>'})"></span>
-            <span ng-init='wxSDK(<?= Yii::$app->wx->js->config([
-                'hideMenuItems',
-                'onMenuShareTimeline',
-                'onMenuShareAppMessage'
-            ]) ?>, "<?= $shareTitle ?>", "<?= $shareDescription ?>", "<?= $shareCover ?>")'></span>
-        </div>
-    </div>
+<!-- Footer -->
+<div class="hidden">
+    <span ng-init="common({message: '<?= $app->session->getFlash("message") ?>'})"></span>
+    <span ng-init='wxSDK(<?= $app->wx->js->config([
+        'hideMenuItems',
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage'
+    ]) ?>, "<?= $shareTitle ?>", "<?= $shareDescription ?>", "<?= $shareCover ?>")'></span>
+</div>
 
-    <!-- Menu -->
-    <div class="menu-lm">
-        <p class="menuclose">KAKE</p>
-        <form class="search" id="box" action="/">
-            <input type="hidden" name="r" value="items/index">
-            <input type="search" name="keyword" placeholder="目的地">
-            <!-- <img src="<?= $params['frontend_source'] ?>/img/search.svg"> -->
-        </form>
-        <a href="<?= Url::to(['site/index']) ?>">
-            <img src="<?= $params['frontend_source'] ?>/img/site.svg"/>
-            首页
+<!-- Menu -->
+<div class="menu-lm">
+    <p class="menuclose">KAKE</p>
+    <form class="search" id="box" action="/">
+        <input type="hidden" name="r" value="items/index">
+        <input type="search" name="keyword" placeholder="目的地">
+        <!-- <img src="<?= $params['frontend_source'] ?>/img/search.svg"> -->
+    </form>
+    <a href="<?= Url::to(['site/index']) ?>">
+        <img src="<?= $params['frontend_source'] ?>/img/site.svg"/>
+        首页
+    </a>
+    <a href="<?= Url::to(['order/index']) ?>" class="hr">
+        <img class="order-center" src="<?= $params['frontend_source'] ?>/img/order-center.svg"/>
+        订单中心
+    </a>
+    <?php if (!empty($this->params['user_info']->role) && $this->params['user_info']->role <= 10): ?>
+        <a href="<?= Url::to(['producer/index']) ?>" class="hr">
+            <img src="<?= $params['frontend_source'] ?>/img/producer.svg"/>
+            分销管理
         </a>
-        <a href="<?= Url::to(['order/index']) ?>" class="hr">
-            <img class="order-center" src="<?= $params['frontend_source'] ?>/img/order-center.svg"/>
-            订单中心
-        </a>
-        <?php if (!empty($this->params['user_info']->role) && $this->params['user_info']->role <= 10): ?>
-            <a href="<?= Url::to(['producer/index']) ?>" class="hr">
-                <img src="<?= $params['frontend_source'] ?>/img/producer.svg"/>
-                分销管理
-            </a>
-        <?php endif; ?>
-        <a href="tel:<?= $params['company_tel'] ?>" class="hr">
-            <img src="<?= $params['frontend_source'] ?>/img/phone.svg"/>
-            咨询客服
-        </a>
-        <a href="<?= SsoClient::$ssoHost ?><?= Url::toRoute(['auth/logout']) ?>" class="hr">
-            <img src="<?= $params['frontend_source'] ?>/img/exit.svg"/>
-            退出登录
-        </a>
-        <a href="<?= Url::to(['site/index']) ?>">
-            <img src="<?= $params['frontend_source'] ?>/img/site.svg"/>
-            首页
-        </a>
-        <a href="<?= Url::to(['order/index']) ?>" class="hr">
-            <img class="order-center" src="<?= $params['frontend_source'] ?>/img/order-center.svg"/>
-            订单中心
-        </a>
-        <?php if (!empty($this->params['user_info']->role) && $this->params['user_info']->role <= 10): ?>
-            <a href="<?= Url::to(['producer/index']) ?>" class="hr">
-                <img src="<?= $params['frontend_source'] ?>/img/producer.svg"/>
-                分销管理
-            </a>
-        <?php endif; ?>
-        <a href="tel:<?= $params['company_tel'] ?>" class="hr">
-            <img src="<?= $params['frontend_source'] ?>/img/phone.svg"/>
-            咨询客服
-        </a>
-        <a href="<?= SsoClient::$ssoHost ?><?= Url::toRoute(['auth/logout']) ?>" class="hr">
-            <img src="<?= $params['frontend_source'] ?>/img/exit.svg"/>
-            退出登录
-        </a>
-    </div>
+    <?php endif; ?>
+    <a href="tel:<?= $params['company_tel'] ?>" class="hr">
+        <img src="<?= $params['frontend_source'] ?>/img/phone.svg"/>
+        咨询客服
+    </a>
+    <a href="<?= SsoClient::$ssoHost ?><?= Url::toRoute(['auth/logout']) ?>" class="hr">
+        <img src="<?= $params['frontend_source'] ?>/img/exit.svg"/>
+        退出登录
+    </a>
+</div>
 
 </body>
+
 <script>
+    /*
     var _hmt = _hmt || [];
     (function () {
         var hm = document.createElement("script");
@@ -192,6 +171,7 @@ $shareCover = empty($params['share_cover']) ? $params['frontend_source'] . '/img
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(hm, s);
     })();
+    */
 </script>
 </html>
 <?php $this->endPage() ?>
