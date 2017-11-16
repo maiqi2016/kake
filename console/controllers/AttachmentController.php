26a0cd9eb7f2d0479545a0bfb322965d111f043f
Leon <jiangxilee@gmail.com>
2017-11-16 14:05:34 +0800

    add_console_for_oss

--- console/controllers/AttachmentController.php
+++ console/controllers/AttachmentController.php
@@ -4,8 +4,8 @@ namespace console\controllers;
 
 use common\components\Helper;
 use console\models\Attachment;
+use console\models\ProductDescription;
 use Yii;
-use yii\helpers\Console;
 
 /**
  * Attachment mission
@@ -34,6 +34,7 @@ class AttachmentController extends GeneralController
         $params = [];
         switch ($actionID) {
             case 'upload-to-oss' :
+            case 'update-text-for-oss' :
                 $params = ['limit'];
                 break;
         }
@@ -73,6 +74,47 @@ class AttachmentController extends GeneralController
         }, $model, [], [
             'deep_path',
             'filename'
+        ], $this->limit);
+    }
+
+    /**
+     * Update Content for OSS
+     *
+     * @access public
+     */
+    public function actionUpdateTextForOss()
+    {
+        $model = new ProductDescription();
+
+        $this->missionProgressForTable(function ($record) use ($model) {
+
+            $reg = '/http:\/\/pic\.kakehotels\.com\/upload\/kake\/(\d+)\/(\d+)\/(.*)\.(jpg|jpeg|png|gif)/iU';
+            foreach ($record as $item) {
+                $handler = function ($k) use ($item, $reg) {
+                    $text = $item[$k];
+                    preg_match_all($reg, $text, $match);
+
+                    foreach ($match[0] as $key => $src) {
+                        $newSrc = 'http://pic.kakehotels.com/' . $match[1][$key] . '-' . $match[2][$key] . '-' . $match[3][$key] . '.' . $match[4][$key];
+                        $text = str_replace($src, $newSrc, $text);
+                    }
+
+                    return $text;
+                };
+
+                $model::updateAll([
+                    'cost' => $handler('cost'),
+                    'recommend' => $handler('recommend'),
+                    'use' => $handler('use'),
+                    'back' => $handler('back'),
+                ], ['id' => $item['id']]);
+            }
+        }, $model, [], [
+            'id',
+            'cost',
+            'recommend',
+            'use',
+            'back'
         ], $this->limit);
     }
 }
