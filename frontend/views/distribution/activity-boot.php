cdea14a46cb3db77eddd3c8b227d65a59f8b4e67
cptbtptpe <1159882790@qq.com>
2017-12-25 17:41:20 +0800

    抽奖页面3

--- frontend/views/distribution/activity-boot.php
+++ frontend/views/distribution/activity-boot.php
@@ -26,25 +26,25 @@ $params = \Yii::$app->params;
 <a class="detail" href="<?= $prize['link_url'] ?>"> >>点击查看套餐详情<< </a>
 <div class="blank"></div>
 <div class="hot-list">
-    <a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/bichizizhu.gif"></a>
+    <a href="<?= Url::toRoute(['items/index']) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/hot1.png"></a>
     <ul>
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/bichizizhu.gif"></a></li>
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/bichizizhu.gif"></a></li>
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/bichizizhu.gif"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index', 'classify' => 0]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/hotel.png"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index', 'classify' => 1]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/eat.png"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index', 'classify' => 2]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/play.png"></a></li>
     </ul>
 </div>
 <div class="destination">
-    <a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/bichizizhu.gif"></a>
+    <a href="<?= Url::toRoute(['items/region']) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/hot2.png"></a>
     <ul class="clearfix">
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/banner1.png"></a></li>
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/banner1.png"></a></li>
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/banner1.png"></a></li>
-        <li><a href="#"><img src="<?= $params['frontend_source'] ?>/img/banner1.png"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index','plate' => 3]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/1.jpg"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index','plate' => 4]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/2.jpg"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index','plate' => 9]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/3.jpg"></a></li>
+        <li><a href="<?= Url::toRoute(['items/index','plate' => 5]) ?>"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/4.jpg"></a></li>
     </ul>
 </div>
 
 <div class="qr-code">
-    <a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/bichizizhu.gif"></a>
+    <a href="#"><img src="<?= $params['frontend_source'] ?>/img/distribution/activity-boot/qr.png"></a>
 </div>
 
 <div class="kk-modal">
