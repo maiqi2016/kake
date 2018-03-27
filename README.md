d8c69dd024d626cbc9f9dff2425685636f45f4f5
admin <admin@leon.lan>
2018-03-27 14:57:15 +0800

    modifiy_readme_02

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# KAKE
-Trip app
+# 项目
+喀客酒店预订平台
 
-## Install step
-* git clone https://github.com/maiqi2016/kake.git
-* cd kake
-* chmod a+x install.sh
-* ./install.sh
+## 安装步骤
+
+```shell
+$ git clone https://github.com/maiqi2016/kake.git
+$ chmod a+x kake/install.sh
+```
+
+### 本机环境
+
+```shell
+$ cd kake
+$ composer install
+$ ./install.sh
+```
+
+### `Docker` 环境
+
+```
+$ sudo docker-compose up -d     # 并确保已经安装(执行)了 `/web/docker/script/` 目录下的所有脚本
+$ mq-composer install --ignore-platform-reqs
+$ mq-bash kake/install.sh
+$ mq-bash
+(docker)$ /usr/local/init-service
+```
--- install.sh
+++ install.sh
@@ -1,37 +1,38 @@
 #! /bin/bash
 
-# composer
-composer install
+path=$(cd `dirname $0`; pwd)
 
 # create config files
-sudo cp common/config/main.php.backup common/config/main.php
-sudo cp common/config/main-local.php.backup common/config/main-local.php
-sudo cp common/config/params.php.backup common/config/params.php
-sudo cp common/config/params-local.php.backup common/config/params-local.php
+cp ${path}/common/config/main.php.backup ${path}/common/config/main.php
+cp ${path}/common/config/main-local.php.backup ${path}/common/config/main-local.php
+cp ${path}/common/config/params.php.backup ${path}/common/config/params.php
+cp ${path}/common/config/params-local.php.backup ${path}/common/config/params-local.php
 
-sudo cp backend/config/main.php.backup backend/config/main.php
-sudo cp backend/config/main-local.php.backup backend/config/main-local.php
-sudo cp backend/config/params.php.backup backend/config/params.php
-sudo cp backend/config/params-local.php.backup backend/config/params-local.php
+cp ${path}/backend/config/main.php.backup ${path}/backend/config/main.php
+cp ${path}/backend/config/main-local.php.backup ${path}/backend/config/main-local.php
+cp ${path}/backend/config/params.php.backup ${path}/backend/config/params.php
+cp ${path}/backend/config/params-local.php.backup ${path}/backend/config/params-local.php
 
-sudo cp frontend/config/main.php.backup frontend/config/main.php
-sudo cp frontend/config/main-local.php.backup frontend/config/main-local.php
-sudo cp frontend/config/params.php.backup frontend/config/params.php
-sudo cp frontend/config/params-local.php.backup frontend/config/params-local.php
+cp ${path}/frontend/config/main.php.backup ${path}/frontend/config/main.php
+cp ${path}/frontend/config/main-local.php.backup ${path}/frontend/config/main-local.php
+cp ${path}/frontend/config/params.php.backup ${path}/frontend/config/params.php
+cp ${path}/frontend/config/params-local.php.backup ${path}/frontend/config/params-local.php
 
-sudo cp console/config/main.php.backup console/config/main.php
-sudo cp console/config/main-local.php.backup console/config/main-local.php
-sudo cp console/config/params.php.backup console/config/params.php
-sudo cp console/config/params-local.php.backup console/config/params-local.php
+cp ${path}/console/config/main.php.backup ${path}/console/config/main.php
+cp ${path}/console/config/main-local.php.backup ${path}/console/config/main-local.php
+cp ${path}/console/config/params.php.backup ${path}/console/config/params.php
+cp ${path}/console/config/params-local.php.backup ${path}/console/config/params-local.php
 
 # add write
-sudo chmod -R a+w common/config/
-sudo chmod -R a+w console/config/
-sudo chmod -R a+w mixed/
-sudo chmod -R a+w backend/runtime/
-sudo chmod -R a+w frontend/runtime/
-sudo chmod -R a+w backend/web/assets/
-sudo chmod -R a+w frontend/web/assets/
+chmod -R a+w ${path}/common/config/
+chmod -R a+w ${path}/console/config/
+chmod -R a+w ${path}/mixed/
+chmod -R a+w ${path}/backend/runtime/
+chmod -R a+w ${path}/frontend/runtime/
+mkdir ${path}/backend/web/assets/ 2> /dev/null
+chmod -R a+w ${path}/backend/web/assets/
+mkdir ${path}/frontend/web/assets/ 2> /dev/null
+chmod -R a+w ${path}/frontend/web/assets/
 
 echo
 read -p "Please choose environment. [dev/prod]: " env
@@ -41,8 +42,8 @@ then
     exit 1
 fi
 
-sudo cp yii-${env} yii
-sudo chmod a+x yii
+cp ${path}/yii-${env} ${path}/yii
+chmod a+x ${path}/yii
 
-sudo cp backend/web/index-${env}.php backend/web/index.php
-sudo cp frontend/web/index-${env}.php frontend/web/index.php
+cp ${path}/backend/web/index-${env}.php ${path}/backend/web/index.php
+cp ${path}/frontend/web/index-${env}.php ${path}/frontend/web/index.php
